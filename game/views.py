3e56c4248030a9d4c97a37a70e8b3256e4c367be
scy <shuchunyu729@163.com>
2023-08-15 16:33:44 +0800

    first page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style = "text-align: center">狂扁小朋友</h1>'
+    line2 = '<img src = "https://i.ssjz8.com/upload/1/img2.baidu.com%2Fit%2Fu%3D137719425%2C2773523159%26amp%3Bfm%3D253%26amp%3Bfmt%3Dauto%26amp%3Bapp%3D138%26amp%3Bf%3DJPEG%3Fw%3D480%26amp%3Bh%3D272" width = 1500>'
+    return HttpResponse(line1 + line2)
